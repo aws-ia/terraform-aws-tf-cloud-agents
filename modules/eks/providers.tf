e53136969129a1b76b24a3f98b575978b7cc71b2
Gautam <gautambaghel93@gmail.com>
2024-05-28 14:00:08 -0700

    changing the module invocation

--- modules/eks/providers.tf
+++ modules/eks/providers.tf
@@ -22,15 +22,15 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = var.create_eks_cluster ? module.eks.cluster_endpoint : var.cluster_endpoint_url
-  cluster_ca_certificate = var.create_eks_cluster ? base64decode(module.eks.cluster_certificate_authority_data) : var.cluster_certificate_authority_data
+  host                   = var.create_eks_cluster ? module.eks[0].cluster_endpoint : var.cluster_endpoint_url
+  cluster_ca_certificate = var.create_eks_cluster ? base64decode(module.eks[0].cluster_certificate_authority_data) : var.cluster_certificate_authority_data
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = var.create_eks_cluster ? module.eks.cluster_endpoint : var.cluster_endpoint_url
-    cluster_ca_certificate = var.create_eks_cluster ? base64decode(module.eks.cluster_certificate_authority_data) : var.cluster_certificate_authority_data
+    host                   = var.create_eks_cluster ? module.eks[0].cluster_endpoint : var.cluster_endpoint_url
+    cluster_ca_certificate = var.create_eks_cluster ? base64decode(module.eks[0].cluster_certificate_authority_data) : var.cluster_certificate_authority_data
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
