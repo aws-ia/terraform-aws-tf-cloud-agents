4bd2813a712970d84602bf2b95463374254ca87d
wellsiau-aws <wellsiau@amazon.com>
2024-06-03 19:36:28 -0700

    prep functional test

--- .config/functional_tests/pre-entrypoint-helpers.sh
+++ .config/functional_tests/pre-entrypoint-helpers.sh
@@ -4,3 +4,16 @@
 ## TIPS: avoid modifying the .project_automation/functional_test/entrypoint.sh
 ## migrate any customization you did on entrypoint.sh to this helper script
 echo "Executing Pre-Entrypoint Helpers"
+
+#********** TFC Env Vars *************
+export AWS_DEFAULT_REGION=us-east-1
+export TFE_TOKEN=`aws secretsmanager get-secret-value --secret-id abp/hcp/token | jq -r ".SecretString"`
+
+#********** Get tfvars from SSM *************
+echo "Get *.tfvars from SSM parameter"
+aws ssm get-parameter \
+  --name "/abp/hcp/functional/terraform_test.tfvars" \
+  --with-decryption \
+  --query "Parameter.Value" \
+  --output "text" \
+  --region "us-east-1" >> ./tests/terraform.auto.tfvars
